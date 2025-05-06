122fc57d04d7e9d7b0d3ee8f52f27e59c572ca68
Limsungyong <tjddy236@naver.com>
2025-05-06 17:44:48 +0900

    캘린더 페이지 진행도 바 구현

--- mobile_project/src/route/calendar/CalendarPage.jsx
+++ mobile_project/src/route/calendar/CalendarPage.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import './CalendarPage.css'
-
+import { useState, useEffect } from 'react';
+import { ProgressBar } from 'react-bootstrap';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+import './CalendarPage.css';
 function CalendarPage()
 {
 
   return (
-    <>
-        <h1>캘린더 화면</h1>
-    </>
-  )
+    <div className="calendar-content">
+        { /*테스트용 진행도바*/ }
+        <div style={{width:"90%", marginTop:"20px"}}>
+          <p>성취율</p>
+          <ProgressBar now={80} label={`${80}%`} /> {/*성취율 값 넣기 (now,lable)*/}
+        </div>
+    </div>
+  );
 }
 
 export default CalendarPage
