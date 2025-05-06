4b30d529579c1c8d2800aa5032fb5659d473dd79
Yongb15 <dydqls5658@naver.com>
2025-05-06 15:30:03 +0900

    develop에서 pull

--- mobile_project/dev-dist/sw.js
+++ mobile_project/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sfihinde6k"
+    "revision": "0.cv79i9qnbj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
