18825aa9f92c6da9e82e6635ab44c97ee24efca7
Seungho1201 <ohseungho1201@gmail.com>
2025-05-06 14:45:53 +0900

    플래너날짜계산이펙트

--- mobile_project/dev-dist/sw.js
+++ mobile_project/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bnp283hm75g"
+    "revision": "0.vp1gaktu63"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- mobile_project/src/route/planpage/PlanPage.jsx
+++ mobile_project/src/route/planpage/PlanPage.jsx
@@ -1,9 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import './PlanPage.css'
 
 function PlanPage()
 {
+  const [planData, setPlanData] = useState([]);   // 초기값을 빈 배열로 설정
+  const [startDay, setStartDay] = useState('');   // 시작 날짜
+  const [finalDay, setFinalDay] = useState('');   // 마지막 날짜 (시작날짜로부터 + 3)
+  const [inputPlan, setInputPlan] = useState(''); // 입력창에 쓸 임시 계획
+  const [planList, setPlanList] = useState([]);   // 플랜 항목 목록
+  const [clear, setClear] = useState([false, false, false]); // 각 날의 완료 여부
+
+  // 시작 날짜(StartDay)가 변할 때마다 종료 날짜(finalDay)를 자동으로 계산
+  useEffect(() => {
+    // startDay State 값이 있을때만 실행
+    if (startDay) {
+      const date = new Date(startDay);                // date 객체 생성
+
+      date.setDate(date.getDate() + 2);               // 시작일 포함 3일 후 = +2일
+      const final = date.toISOString().split('T')[0]; // yyyy-mm-dd 형식으로 
+
+      setFinalDay(date.toISOString().split('T')[0]);  // finalDay State Set 
+    } else {
+      setFinalDay('');
+    }
+  }, [startDay]);
 
   return (
     <>
