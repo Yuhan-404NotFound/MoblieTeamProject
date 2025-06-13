9e2e8c46baeca9fa2f80f238bf7d36bf20091e7b
Seungho1201 <ohseungho1201@gmail.com>
2025-06-13 15:36:25 +0900

    설계파일 ㅍ첨부

--- mobile_project/dev-dist/sw.js
+++ mobile_project/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d880l5a4up8"
+    "revision": "0.a4hupf5kmhg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
